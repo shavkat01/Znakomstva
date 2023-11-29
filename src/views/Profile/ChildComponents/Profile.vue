60e490b1071dac6e9dcfca2e9efd1bca9a5d178d
shavkat01 <sultonov7881@gmail.com>
2023-11-29 15:21:11 +0500

    change

--- src/views/Profile/ChildComponents/Profile.vue
+++ src/views/Profile/ChildComponents/Profile.vue
@@ -1,8 +1,12 @@
 <script setup>
     import {ref} from 'vue'
     import MesaageInput from '@/components/CustomInput.vue'
+
+    import {windowUiStore} from '@/stores/window-ui'
+
     import InputSelect from '@/components/InputSelect.vue'
-        
+
+    const windowUiStorage = windowUiStore()
     const open_dialog = ref(false)
     const gender = ref('man')
 
@@ -13,25 +17,25 @@
 
 
     <div class="w-full md:mt-0 mt-5">
-        <h1 class="font-semibold text-[28px]">Профиль</h1>
+        <h1 class="md:block hidden font-semibold text-[28px]">Профиль</h1>
         <div class="w-full mt-5 md:flex gap-4">
-            <div class="w-[30%]">
-                <img class="w-[280px]" src="@/assets/images/candidate-main.svg" alt="ded">
+            <div class="md:w-[30%]">
+                <img class="md:w-[280px]" src="@/assets/images/candidate-main.svg" alt="ded">
                 <div class="flex justify-between mt-3">
                     <img class="p-6 border rounded-[30px] border-[#925FA4]" src="@/assets/icons/camera1.svg" alt="ded">
                     <img class="p-6 border rounded-[30px] border-[#925FA4]" src="@/assets/icons/camera1.svg" alt="ded">
                     <img class="p-6 border rounded-[30px] border-[#925FA4]" src="@/assets/icons/camera1.svg" alt="ded">
                 </div>
             </div>
-            <div class="w-[68%]">
-                <h3 class="font-semibold text-[18px]">Основная информация</h3>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Имя</h4>
-                    <MesaageInput placeholder="Ваше имя"/>
+            <div class="md:w-[68%]">
+                <h3 class="font-semibold md:mt-0 mt-5 text-[18px]">Основная информация</h3>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Имя</h4>
+                    <MesaageInput :class="windowUiStorage.window_size < 768 ? 'w-full' : ''" placeholder="Ваше имя"/>
                 </div>
-                <div class="flex justify-between items-center mt-4">
-                    <h4>Дата рождения</h4>
-                    <div>
+                <div class="md:flex justify-between items-center mt-4">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Дата рождения</h4>
+                    <div class="md:block flex">
                         <MesaageInput class="w-[100px]" placeholder="День"/>
                         <div class="mx-1 inline-flex w-[142px]">
                             <InputSelect 
@@ -54,8 +58,8 @@
                         <MesaageInput class="w-[100px]" placeholder="Год"/>
                     </div>
                 </div>
-                  <div class="flex justify-between items-center mt-4">
-                    <h4>Имя</h4>
+                  <div class="md:flex justify-between items-center mt-4">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Имя</h4>
                     <div class="w-[346px] flex gap-2">
                         <button @click="gender = 'man'" :style="gender == 'man' ? 'background-image: linear-gradient(to right, #0575E6, #021B79);' : ''" 
                         class="custom-btn flex justify-center w-[49%] py-[10px]">
@@ -68,49 +72,49 @@
                     </div>
                 </div>
                 <h3 class="font-semibold text-[18px] mt-6">Личная информация</h3>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Рост</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Рост</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <InputSelect :customSelect="true"/>
                     </div>
                 </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Вес</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Вес</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <InputSelect :customSelect="true"/>
                     </div>
                 </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Цвет волос</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Цвет волос</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <InputSelect :customSelect="true"/>
                     </div>
                 </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Цвет глаз</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Цвет глаз</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <InputSelect :customSelect="true"/>
                     </div>
                 </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>Цель знакомства</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Цель знакомства</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <InputSelect :items="[{name:'Пока не знаю'}]" :customSelect="true"/>
                     </div>
                 </div>
-                <div class="flex justify-between mt-3">
-                    <h4>О себе</h4>
+                <div class="md:flex justify-between mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">О себе</h4>
                     <div class="mx-1 inline-flex w-[342px]">
                         <textarea class="bg-[#42204E] rounded-[30px] outline-none p-4 px-5" placeholder="Напишите о себе" cols="30" rows="6"></textarea>
                     </div>
                 </div>
                 <h3 class="font-semibold text-[18px] mt-5">Аккаунт</h3>
-                <div class="flex justify-between items-center mt-3">
-                    <h4>E-mail</h4>
+                <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">E-mail</h4>
                     <MesaageInput placeholder="Введите e-mail"/>
                 </div>
-                 <div class="flex justify-between items-center mt-3">
-                    <h4>Пароль</h4>
+                 <div class="md:flex justify-between items-center mt-3">
+                    <h4 class="md:m-0 md:text-[#FFF] mb-2 ml-5 text-[#6E447B]">Пароль</h4>
                     <MesaageInput :eyeIcon="true" placeholder="Введите пароль"/>
                 </div>
                 <div class="text-end mt-3">
